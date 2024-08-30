e28a359d78a8e001136f3a2cc428a0dd2207b567
DESKTOP-25TV5SJ\BigData <jeppi9944@gmail.com>
2024-08-30 14:34:49 +0900

    test2 url 매핑 구현

--- src/main/java/com/study/springboot/MyController.java
+++ src/main/java/com/study/springboot/MyController.java
@@ -10,13 +10,19 @@ public class MyController
 	
 	@RequestMapping("/")
 	public @ResponseBody String root() throws Exception{
-		return "menu";
+		return "JSP in Gradle";
 	}
 	
 	@RequestMapping("/test1")
 	public String test1() {
 		
 		return "test1";
+		
+	}
+	@RequestMapping("/test2")
+	public String test2() {
+		
+		return "test2";
 		
 	}
 	
